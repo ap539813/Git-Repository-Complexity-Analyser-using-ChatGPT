307acbd966f07c171eb17be991d1e84760ba05e2
Ankush Pandey <ap539813@gmail.com>
2023-06-18 14:39:52 +0530

    initialised the language model in session

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import altair as alt
 import streamlit as st
-import seaborn as sns
-
+import json
+import os
 # importing the local modules
+from important_variables import model_name
 from application_pages import main, homepage
+
+import openai
+# from langchain.llms import OpenAI
+# import langchain.llms
+from langchain.chat_models import ChatOpenAI
 
 st.set_page_config(layout="wide")
 
 alt.renderers.set_embed_options(scaleFactor=2)
 
+with open('secrets.json') as f:
+    data = json.load(f)
+
+os.environ["OPENAI_API_KEY"] = data['api_key']
+
+st.session_state['GPT'] = ChatOpenAI(temperature=0.7, model=model_name, )
 
 
 if 'home_page' not in st.session_state:
